c7b66d5fbc59b68954c23949edfbbc45f7f85feb
naeramit14 <naeramit.rid.dev@gmail.com>
2024-07-28 21:39:18 +0700

    add step

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Container from "./components/Container";
 
+
 function App() {
   return (
     <div className="w-screen  h-screen flex  justify-end items-end bg-gray-100 p-4">
--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,16 +1,44 @@
 import { useState } from "react";
+import { Steps } from "antd";
 import Header from "./Header";
 
 function Index() {
   const [open, setOpen] = useState(true);
+  const [current, setCurrent] = useState(0);
 
   const handleOpenChat = () => {
     setOpen(!open);
   };
+
+  const handleOnchangeStep = (value) => {
+    setCurrent(value);
+  };
   return (
     <div className="w-[390px] max-w-[390px] max-h-[850px] shadow-lg bg-white rounded-t-md">
       <Header handleClick={handleOpenChat} />
-      {open ? <div className="h-[650px] "></div> : null}
+      {open ? (
+        <div className="h-[650px] w-full p-2">
+          <Steps
+            size="small"
+            current={current}
+            onChange={handleOnchangeStep}
+            items={[
+              {
+                title: "",
+              },
+              {
+                title: "",
+              },
+              {
+                title: "",
+              },
+              {
+                title: "",
+              },
+            ]}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
